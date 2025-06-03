5cbc66298cbf3420ef847f03e259c581c83ca00e
Mohammad-Shaikh-Ibrahim <mohammadshaikhibrahim2002@gmail.com>
2025-06-03 10:20:28 +0300

    Updates

---
+++ app.js
@@ -0,0 +1,2 @@
+import{j as r,S as s}from"./chart.js";function n(){return r.jsx(r.Fragment,{children:r.jsxs("div",{className:"App",children:[r.jsx("h1",{children:"Chart"}),r.jsx(s,{})]})})}export{n as A};
+//# sourceMappingURL=app.js.map
